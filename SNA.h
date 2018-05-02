b4d12c033b797017614b40416373c89e302059a2
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2018-05-02 17:31:15 +0200

    Create SNA.h

---
+++ SNA.h
@@ -0,0 +1,55 @@
+#ifndef SNA_H_
+#define SNA_H_
+/*----------------------------------------------------*/
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+/*----------------------------------------------------*/
+
+typedef struct
+{
+	string text;
+	int likes  ;
+	int id     ;
+}Post;
+/*----------------------------------------------------*/
+class User
+{
+protected:
+	string Name    ;
+	string Mail    ;
+	string Password;
+	int    Age     ;
+	vector<Post>Posts     ;
+	vector<string>Friends ;
+public:
+	//User(string,string,string,int);
+	void LikePost(int id)         ;
+
+	void MutualFriends()          ;          // i think it should be added to the System class
+
+	void AddFriend(string mail)   ;
+
+	void AcceptFriend(string mail);
+
+	void Search(string name)      ;          // i think it should be added to the System class
+
+	void PostText(string)         ;
+
+	void ViewUserProfile(string mail)        ;
+
+	void EditUserProfile(string mail)        ;
+
+	void BrowseUserFriends(string mail)      ;
+
+	void Login(string name , string password);
+
+	void Register(string,string,string,int)  ;
+};
+
+
+
+
+#endif /* SNA_H_ */
