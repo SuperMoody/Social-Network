5d9dfdf183d4538e500aa8351b9060d2c7c77319
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2018-05-16 08:02:23 +0200

    Update SNA2.cpp

--- SNA2.cpp
+++ SNA2.cpp
@@ -1,9 +1,9 @@
 #include"SNA.h"
-/*under testing*/
+
 void User::Load(string KeyWord , string mail){}
 void User::Save(string KeyWord , string mail){}
-int UNI_ID;
 
+int UNI_ID;
 
 void User::Load_Post(char* mail,vector<Post>*vec){
 	char file_name[70];
@@ -22,8 +22,8 @@ void User::Load_Post(char* mail,vector<Post>*vec){
 			Post post;
 			Posts.push_back(post);
 		}
-
-
+		Posts.pop_back();
+		Posts.pop_back();
 	}
 	else{
 		string temp(mail);
@@ -41,7 +41,8 @@ void User::Load_Post(char* mail,vector<Post>*vec){
 			Post post;
 			(*vec).push_back(post);
 		}
-
+		(*vec).pop_back();
+		(*vec).pop_back();
 	}
 
 }
@@ -63,8 +64,8 @@ void User::Load_Friends(char* mail,vector<Friend>*vec){
 				Friend  friend1;
 				Friends.push_back(friend1);
 			}
-
-
+			Friends.pop_back();
+			Friends.pop_back();
 		}
 		else{
 			string temp(mail);
@@ -82,7 +83,8 @@ void User::Load_Friends(char* mail,vector<Friend>*vec){
 				Friend friend1;
 				(*vec).push_back(friend1);
 			}
-
+			(*vec).pop_back();
+			(*vec).pop_back();
 		}
 }
 
@@ -107,11 +109,15 @@ void User::Load_Requests(char* mail){
 	Requests.push_back(request);
 	int i =0;
 	while( !file.eof() ){
-		file.read( reinterpret_cast<char*>(&Requests[i]), sizeof(Requests[i]) );
+		file.read( reinterpret_cast<char*>(&Requests[i]), sizeof(Requests[i]));
 		i++;
-		Request friend1;
-		Requests.push_back(friend1);
+		Request request1;
+		Requests.push_back(request1);
 		}
+	if(mail==NULL){
+		Requests.pop_back();
+		Requests.pop_back();
+	}
 }
 
 
@@ -119,10 +125,13 @@ void User::Load_Requests(char* mail){
 void User::Save_Post(char* mail,vector<Post>*vec){
 	char file_name[70];
 	if(mail==NULL){
+
+
 		string temp=Mail.substr(0,Mail.find('@'))+"Posts.Dat";
 		strcpy(file_name,temp.c_str());
 		fstream file;
-		file.open(file_name,ios::out | ios::in| ios::app | ios::binary);
+		file.open(file_name,ios::out|ios::trunc| ios::binary);
+		file.seekg(0);
 		for(unsigned int i =0 ; i< Posts.size();i++ ){
 			file.write( reinterpret_cast<char*>(&Posts[i]), sizeof(Posts[i]) );
 		}
@@ -133,7 +142,7 @@ void User::Save_Post(char* mail,vector<Post>*vec){
 		temp=temp.substr(0,temp.find('@'))+"Posts.Dat";
 		strcpy(file_name,temp.c_str());
 		fstream file;
-		file.open(file_name,ios::out | ios::in| ios::app | ios::binary);
+		file.open(file_name,ios::out | ios::trunc |ios::binary);
 		for(unsigned int i =0 ; i< (*vec).size();i++ ){
 			file.write( reinterpret_cast<char*>(&((*vec)[i])), sizeof((*vec)[i]) );
 		}
@@ -149,7 +158,7 @@ void User::Save_Friends(char* mail,vector<Friend>*vec){
 			string temp=Mail.substr(0,Mail.find('@'))+"Friend.Dat";
 			strcpy(file_name,temp.c_str());
 			fstream file;
-			file.open(file_name,ios::out | ios::in| ios::app | ios::binary);
+			file.open(file_name,ios::out | ios::in| ios::binary);
 			for(unsigned int i =0 ; i< Posts.size();i++ ){
 				file.write( reinterpret_cast<char*>(&Friends[i]), sizeof(Friends[i]) );
 			}
@@ -160,7 +169,7 @@ void User::Save_Friends(char* mail,vector<Friend>*vec){
 			temp=temp.substr(0,temp.find('@'))+"Friend.Dat";
 			strcpy(file_name,temp.c_str());
 			fstream file;
-			file.open(file_name,ios::out | ios::in| ios::app | ios::binary);
+			file.open(file_name,ios::out | ios::in| ios::binary);
 			for(unsigned int i =0 ; i< (*vec).size();i++ ){
 				file.write( reinterpret_cast<char*>(&((*vec)[i])), sizeof((*vec)[i]) );
 			}
@@ -181,7 +190,7 @@ void User::Save_Requests(char* mail){
 			strcpy(file_name,temp.c_str());
 		}
 		fstream file;
-		file.open(file_name,ios::out |ios::app| ios::in | ios::binary);
+		file.open(file_name,ios::out| ios::in | ios::binary);
 		for(unsigned int i =0 ; i< Requests.size();i++ ){
 				file.write( reinterpret_cast<char*>(&Requests[i]), sizeof(Requests[i]) );
 		}
@@ -190,29 +199,47 @@ void User::Save_Requests(char* mail){
 
 
 void User::Like_Post(int id,char * mail){
-	Load_Post();
+	//Load_Post();                           //added for testing only
+
 	unsigned int i;
+	string stru(this->Mail);
+	/*modify my posts*/
+
 	for(i =0 ; i< Posts.size();i++)
 	{
 		if(Posts[i].id==id){break;}
 	}
-	Posts[i].id+=1;
+	Posts[i].likes+=1;
+
+
 	Save_Post();
+
 	/*/////////////////////*/
+	/*modify the owner posts*/
+
 	vector<Post>vecp;
 	Load_Post(mail,&vecp);
 	for(i =0 ; i< vecp.size();i++)
 	{
 		if(vecp[i].id==id){break;}
 	}
-	vecp[i].id+=1;
+	vecp[i].likes+=1;
 	Save_Post(mail,&vecp);
 	vecp.clear();
+
 	/*/////////////////////*/
+	/*modify the owner's friend's posts */
+
 	vector<Friend>vecf;
 	Load_Friends(mail,&vecf);
 	for(unsigned int i =0 ; i< vecf.size();i++)
 	{
+
+		string strf(vecf[i].Mail);
+		if(strf==stru)
+			continue;
+
+
 		vector<Post>vecp;
 		Load_Post(vecf[i].Mail,&vecp);
 		unsigned int j;
@@ -220,8 +247,8 @@ void User::Like_Post(int id,char * mail){
 		{
 			if(vecp[j].id==id){break;}
 		}
-		vecp[j].id+=1;
-		Save_Post(mail,&vecp);
+		vecp[j].likes+=1;
+		Save_Post(vecf[i].Mail,&vecp);
 		vecp.clear();
 	}
 
@@ -234,7 +261,14 @@ void User::Add_Friend(char* mail,char*name) {
 	strcpy(file_name,temp.c_str());
 	fstream file;
 	file.open(file_name,ios::out |ios::app| ios::in | ios::binary);
-	Request request(mail,name);
+
+	char MAIL[70];
+	char NAME[50];
+
+	strcpy(MAIL,this->Mail.c_str());
+	strcpy(NAME,this->Name.c_str());
+
+	Request request(MAIL,NAME);
 	file.write( reinterpret_cast<char*>(&request), sizeof(request) );
 
 }
@@ -268,27 +302,44 @@ void User::Accept_Friend(char* mail,char*name){
 
 
 	Friend friend2(name,mail);
-	file1.write( reinterpret_cast<char*>(&friend1), sizeof(friend1) );
+	file1.write( reinterpret_cast<char*>(&friend2), sizeof(friend2) );
 
 }
+
+
 void User::Post_Text(char* text){
 
 	/*CREATE A  POST && and add it to your posts*/
-	Load_Post();
-	string str=this->Mail;
+
+	// i should add  the post to the vector:::done
+
 	char mail[70];
+	string str=this->Mail;
 	strcpy(mail,str.c_str());
 	Post NewPost(mail,text,0,++ UNI_ID);
 	Posts.push_back(NewPost);
-	Save_Post();
+	char file_name[70];
+	str=str.substr(0,str.find('@'))+"Posts.Dat";
+	strcpy(file_name,str.c_str());
+
+	fstream file;
+	file.open(file_name,ios::out |ios::app| ios::in | ios::binary);
+	file.write( reinterpret_cast<char*>(&NewPost), sizeof(NewPost) );
+
+
 	/*--------------------------------------------------------*/
 	/*send post to friends*/
-	Load_Friends();
+	//Load_Friends();									//for testing only
+
 	for(unsigned int i = 0 ; i<Friends.size(); i++ ){
-		vector<Post>vec;
-		Load_Post(Friends[i].Mail,&vec);
-		Save_Post(Friends[i].Mail,&vec);
-		vec.clear();
+			char file_name1[70];
+			string temp(Friends[i].Mail);
+			temp=temp.substr(0,temp.find('@'))+"Posts.Dat";
+			strcpy(file_name1,temp.c_str());
+
+			fstream file1;
+			file1.open(file_name1,ios::out |ios::app| ios::in | ios::binary);
+			file1.write( reinterpret_cast<char*>(&NewPost), sizeof(NewPost) );
 	}
 }
 /*-------------------------------------------------------------------*/
@@ -343,12 +394,6 @@ void User::Clear_Data(char*mail,char*keyword){
 		remove(file_name);
 	}
 }
-
-
-
-
-
-
 
 
 
